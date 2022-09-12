b724d023f956f5f7275d5adfa87703c619e17b36
alphaEmpyrean <dev@alphaempyrean.com>
2022-09-12 13:21:04 -0500

    Impliment Login

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
 import { SignupRequestPayload } from '../signup/signup-request.payload';
-import { Observable } from 'rxjs';
+import { LoginRequestPayload } from '../login/login-request.payload';
+import { LoginResponse } from '../login/login-response.payload';
+import { LocalStorageService } from 'src/app/shared/local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+    private localStorage: LocalStorageService) { }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
     return this.httpClient.post('http://localhost:8080/api/auth/signup', signupRequestPayload, {responseType: 'text'});
+  }
+
+  login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequestPayload)
+      .pipe(map(data => {
+        this.localStorage.set('authenticationToken', data.authenticationToken);
+        this.localStorage.set('username', data.username);
+        this.localStorage.set('refreshToken', data.refreshToken);
+        this.localStorage.set('expiresAt', data.expiresAt.toString());
+
+        return true;
+      }));
+
   }
 }
